6d98fa108d5d59dd518d37cc5995ba13ab9dc963
Emma Morales <developeremma@developeremma-macbookpro.roam.corp.google.com>
2018-03-05 12:25:42 -0800

    db setup

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+-- Inside your burger directory, create a folder named db.
+
+-- In the db folder, create a file named schema.sql. Write SQL queries this file that do the following:
+
+-- Create the burgers_db.
+-- Switch to or use the burgers_db.
+-- Create a burgers table with these fields:
+-- id: an auto incrementing int that serves as the primary key.
+-- burger_name: a string.
+-- devoured: a boolean. 
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Still in the db folder, create a seeds.sql file. In this file, write insert queries to populate the burgers table with at least three entries.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', false);
